899cea6731a75ea52e37c87daf8c3188c06d86f0
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-09 20:33:30 +0100

    feat: elementi nel DOM esercizio1

--- esercizio1/main.js
+++ esercizio1/main.js
@@ -2,22 +2,24 @@
 
 // Creo un'Array con le mail che possono accedere
 const emailIscritti = ['esempio.uno@gmail.it', 'esempio.due@gmail.it', 'esempio.tre@gmail.it'];
-console.log(emailIscritti)
-// Chiedo all'utente di inserire la sua mail con un prompt
-const emailUtente = (prompt('Inserici la tua email'));
-
-// Verifico con un ciclo che la mail inserita sia presente all'interno dell'Array
-for (let i = 0; i < emailIscritti.length; i++) {
-    // In caso positivo stampo in console "Email trovata" 
-    if (emailUtente === emailIscritti[i]) {
-        console.log('Email Trovata: ', emailUtente);
-        break;
+console.log(emailIscritti);
+// Chiedo all'utente di inserire la sua mail 
+const emailUtente = document.querySelector('#user');
+// Bottone Log in
+const logInButton = document.querySelector('#log-in');
+// Risultato
+const risultato = document.querySelector('.risultato')
+// Evento bottone Log in 
+logInButton.addEventListener("click", function () {
+    const userValue = emailUtente.value
+    // Verifico con un ciclo che la mail inserita sia presente all'interno dell'Array
+    for (let i = 0; i < emailIscritti.length; i++) {
+        // In caso positivo stampo in console e nel DOM "Email trovata" 
+        if (emailIscritti[i] === userValue) {
+            console.log('Email Trovata: ', userValue);
+            risultato.innerHTML = ('Email Trovata')
+            break;
+        }
     }
-    // in caso negativo stampo in console "Email non trovata"
-    else {
-        console.log('Email non Trovata: ', emailUtente);
-        break;
-    }
-
-}
+})
 
