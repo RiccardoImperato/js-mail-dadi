78fb845ee0f5c860dae024776ef4c1b9742614b9
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-09 20:33:56 +0100

    feat: elementi nel DOM esercizio 2

--- esercizio2/main.js
+++ esercizio2/main.js
@@ -1,21 +1,32 @@
 'use strict'
 
-// Chiedo al giocatore di lancire i dadi con un allert
-const start = (alert('Clicca "OK" per lanciare i Dadi'));
-
-// Definisco due variabili punteggio per il giocatore e per il computer
-const giocatore = (Math.floor(Math.random() * 6));
-const computer = (Math.floor(Math.random() * 6));
-console.log('Giocatore: ', giocatore);
-console.log('Computer: ', computer);
-
-// Confronto con una condizione il punteggio più alto
-if (giocatore > computer) {
-    console.log('Hai Vinto');
-}
-else if (giocatore === computer) {
-    console.log('Pareggio');
-}
-else {
-    console.log('Hai Perso')
-}
+// Variabili giocatore, computer e button
+const player = document.querySelector('.player');
+const com = document.querySelector('.com');
+const risultato = document.querySelector('.risultato');
+const startButton = document.querySelector('#start');
+// Evento lancio dadi
+startButton.addEventListener("click", function () {
+    // Punteggio giocatore
+    const giocatore = (Math.floor(Math.random() * 6));
+    // Punteggio computer
+    const computer = (Math.floor(Math.random() * 6));
+    console.log('Giocatore: ', giocatore);
+    console.log('Computer: ', computer);
+    // Valori nel DOM
+    player.innerHTML = giocatore;
+    com.innerHTML = computer;
+    // Condizioni
+    if (giocatore > computer) {
+        console.log('Hai Vinto');
+        risultato.innerHTML = ('Hai Vinto');
+    }
+    else if (giocatore === computer) {
+        console.log('Pareggio');
+        risultato.innerHTML = ('Pareggio');
+    }
+    else {
+        console.log('Hai Perso');
+        risultato.innerHTML = ('Hai Perso');
+    }
+})
